b598692dbf7b59e1efc60413487b34e32d214565
Christiaan Rudolfs <crudolfs@pancompany.com>
2019-03-28 00:46:36 +0100

    initial commit

---
+++ src/main/java/com/rudolfs/archetype/reactive/RxJava2Example.java
@@ -0,0 +1,16 @@
+package com.rudolfs.archetype.reactive;
+
+import io.reactivex.Flowable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RxJava2Example {
+    private static final Logger LOG = LoggerFactory.getLogger(RxJava2Example.class);
+
+    public static void main(String[] args) {
+        Flowable.just("thinking", "reactive", "is", "easy", "with", "RxJava2")
+                .filter(word -> word.contains("a"))
+                .sorted()
+                .subscribe(LOG::info);
+    }
+}
